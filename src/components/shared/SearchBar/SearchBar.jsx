30de3170c423740c15f591e5b6a34c386a7ee675
S4nt1nO <Gianluca1996@gmx.de>
2024-06-17 15:43:06 +0200

    searchbar
    added close button, cause of not clickable bug

--- src/components/shared/SearchBar/SearchBar.jsx
+++ src/components/shared/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import '../SearchBar/SearchBar.scss'
 import { useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
-import lens from '../../../assets/img/vector.svg'
+import lens from '../../../assets/img/lens.svg'
 import { FilterContext } from '../../utils/Contexts/FilterContext.jsx'
 import { InputContext } from '../../utils/Contexts/InputContext.jsx'
 import { MovieContext } from '../../utils/Contexts/MovieContext.jsx'
@@ -15,8 +15,7 @@ function SearchBar() {
   const location = useLocation()
   const [searchFrame, setSearchFrame] = useState(false)
   const [focused, setFocused] = React.useState(false)
-  const onFocus = () => setFocused(true)
-  const onBlur = () => setFocused(false)
+  const [icon, setIcon] = useState('')
 
   useEffect(() => {
     if (inputSearch === '') {
@@ -35,69 +34,67 @@ function SearchBar() {
   }, [inputSearch])
 
   return (
-    <>
-      <section className="searchbar_wrapper">
-        <div className="search_bar">
-          <div>
-            <label htmlFor="search"></label>
-            <input
-              onFocus={onFocus}
-              onBlur={onBlur}
-              type="text"
-              name="search"
-              id="search"
-              placeholder="Search Movie..."
-              onChange={handleInputSearch}
-              value={inputSearch}
-              className="search_input"
-            />
-          </div>
+    <section className="searchbar_wrapper">
+      <div className="search_bar">
+        <div className="input_wrapper">
+          <label htmlFor="search"></label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search Movie..."
+            onChange={handleInputSearch}
+            value={inputSearch}
+            className={searchFrame ? 'close' : 'search_input'}
+          />
+          <div onClick={() => setSearchFrame(false)} className="close_div"></div>
         </div>
-        {location.pathname !== '/home' && location.pathname !== '/popular' ? (
-          <div className="genre_buttons">
-            <NavLink
-              to="/list"
-              className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
-              <button className="button_red" type="button" value="28" onClick={handleGenreSearch}>
-                Action
-              </button>
-            </NavLink>
-            <NavLink
-              to="/list"
-              className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
-              <button className="button_red" value="35" onClick={handleGenreSearch}>
-                Comedy
-              </button>
-            </NavLink>
-            <NavLink
-              to="/list"
-              className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
-              <button className="button_red" value="27" onClick={handleGenreSearch}>
-                Horror
-              </button>
-            </NavLink>
-          </div>
-        ) : null}
-        {location.pathname === '/home' || location.pathname === '/popular' ? (
-          <div className="movie_list_buttoncontainer">
-            <NavLink to="/list">
-              <button type="button" className="button_red">
-                Browse all
-              </button>
-            </NavLink>
-          </div>
-        ) : null}
-        {location.pathname === '/home' ? (
-          <>
-            <section className={searchFrame && focused ? 'searched_movie_frame' : 'hide'}>
-              {movieData.map(movie => (
-                <SearchModalItem key={movie.id} movie={movie} id={movie.id} />
-              ))}
-            </section>
-          </>
-        ) : null}
-      </section>
-    </>
+        <div className="invis_div"></div>
+      </div>
+      {location.pathname !== '/home' && location.pathname !== '/popular' ? (
+        <div className="genre_buttons">
+          <NavLink
+            to="/list"
+            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
+            <button className="button_red" type="button" value="28" onClick={handleGenreSearch}>
+              Action
+            </button>
+          </NavLink>
+          <NavLink
+            to="/list"
+            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
+            <button className="button_red" value="35" onClick={handleGenreSearch}>
+              Comedy
+            </button>
+          </NavLink>
+          <NavLink
+            to="/list"
+            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
+            <button className="button_red" value="27" onClick={handleGenreSearch}>
+              Horror
+            </button>
+          </NavLink>
+        </div>
+      ) : null}
+      {location.pathname === '/home' || location.pathname === '/popular' ? (
+        <div className="movie_list_buttoncontainer">
+          <NavLink to="/list">
+            <button type="button" className="button_red">
+              Browse all
+            </button>
+          </NavLink>
+        </div>
+      ) : null}
+      {location.pathname === '/home' ? (
+        <>
+          <section className={searchFrame ? 'searched_movie_frame' : 'hide'}>
+            {movieData.map(movie => (
+              <SearchModalItem key={movie.id} movie={movie} id={movie.id} />
+            ))}
+          </section>
+        </>
+      ) : null}
+    </section>
   )
 }
 
