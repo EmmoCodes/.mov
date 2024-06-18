008c75737b86bb7c0bdba26312d0135769785900
S4nt1nO <Gianluca1996@gmx.de>
2024-06-18 21:20:19 +0200

    finished

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -27,82 +27,80 @@ function Home() {
   }, [popularMovies])
 
   return (
-    <>
-      <section className="home">
-        <div>
-          <h1>Welcome, Helmut</h1>
-          <SearchBar />
-          <div></div>
-        </div>
-        <section className="slider_wrapper">
-          <div className={'slider_container'}>
-            <div className="trending">
-              <p>Trending Movies</p>
-              <Link to="/popular">
-                <span defaultValue="27">See All</span>
-              </Link>
-            </div>
-            <AwesomeSlider className="aws_btn">
-              {randomMovies.map(movie => (
-                <div key={movie.id}>
-                  <Link to={`/details/${movie.id}`}>
-                    <img
-                      className="image"
-                      src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                      alt="photos"
-                      key={movie.backdrop_path}
-                    />
-                    <div className="rating_container">
-                      <h2>{movie.title.substring(0, 14)}</h2>
-                      <div>
-                        <img src={star} alt="" />
-                        <p>{movie.vote_average.toFixed(1)} / 10.0</p>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </AwesomeSlider>
-          </div>
-        </section>
-        <section className="animated_banner_container hidden_section">
+    <section className="home">
+      <div>
+        <h1>Welcome, Helmut</h1>
+        <SearchBar />
+        <div></div>
+      </div>
+      <section className="slider_wrapper">
+        <div className={'slider_container'}>
           <div className="trending">
             <p>Trending Movies</p>
             <Link to="/popular">
               <span defaultValue="27">See All</span>
             </Link>
           </div>
-          <article className="animated_banner">
+          <AwesomeSlider className="aws_btn">
             {randomMovies.map(movie => (
-              <div key={movie.id} className="image_container">
+              <div key={movie.id}>
                 <Link to={`/details/${movie.id}`}>
                   <img
-                    className="animated_image"
+                    className="image"
                     src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
                     alt="photos"
                     key={movie.backdrop_path}
                   />
-                  <h2>{movie.title}</h2>
+                  <div className="rating_container">
+                    <h2>{movie.title.substring(0, 14)}</h2>
+                    <div>
+                      <img src={star} alt="" />
+                      <p>{movie.vote_average.toFixed(1)} / 10.0</p>
+                    </div>
+                  </div>
                 </Link>
               </div>
             ))}
-            {randomMovies.map(movie => (
-              <div key={movie.id} className="image_container">
-                <Link to={`/details/${movie.id}`}>
-                  <img
-                    className="animated_image"
-                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                    alt="photos"
-                    key={movie.backdrop_path}
-                  />
-                  <h2>{movie.title}</h2>
-                </Link>
-              </div>
-            ))}
-          </article>
-        </section>
+          </AwesomeSlider>
+        </div>
       </section>
-    </>
+      <section className="animated_banner_container hidden_section">
+        <div className="trending">
+          <p>Trending Movies</p>
+          <Link to="/popular">
+            <span defaultValue="27">See All</span>
+          </Link>
+        </div>
+        <article className="animated_banner">
+          {randomMovies.map(movie => (
+            <div key={movie.id} className="image_container">
+              <Link to={`/details/${movie.id}`}>
+                <img
+                  className="animated_image"
+                  src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                  alt="photos"
+                  key={movie.backdrop_path}
+                />
+                <h2>{movie.title}</h2>
+              </Link>
+            </div>
+          ))}
+          {randomMovies.map(movie => (
+            <div key={movie.id} className="image_container">
+              <Link to={`/details/${movie.id}`}>
+                <img
+                  className="animated_image"
+                  src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                  alt="photos"
+                  key={movie.backdrop_path}
+                />
+                <h2>{movie.title}</h2>
+              </Link>
+            </div>
+          ))}
+        </article>
+      </section>
+    </section>
   )
 }
 
